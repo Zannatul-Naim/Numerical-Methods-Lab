aeb53039435dddc5ba0408c2283faef426634059
Zannatul Naim <zannatulnaim09@gmail.com>
2023-07-09 17:12:08 +0600

    Equation solve using gauss elimination

--- Gauss_Elimination.cpp
+++ Gauss_Elimination.cpp
@@ -2,92 +2,63 @@
 
 using namespace std;
 
-class Gauss_Elimination {
+class GaussElimination {
     public:
     void solve() {
-        cout.precision(4);
+
         int n;
-        cout << "Enter number of equations : ";
         cin >> n;
 
-        double a[n][n+1]; // augmented matrix
-
-        cout << "Enter the augmented matrix: " << endl;
+        double a[100][101];
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n+1; j++) {
                 cin >> a[i][j];
-            }
+            } 
         }
 
-        // pivotisation
-        for(int i = 0; i < n; i++) {
-            for(int k = i+1; k < n; k++) {
-                if(fabs(a[i][i]) < fabs(a[k][i])) {
-                    for(int j = 0; j < n; j++) {
-                        swap(a[i][j], a[k][j]);
-                    }
-                }
-            }
-        }
+        // Gauss elimination
 
-        cout << "Matrix after pivotisation: " << endl;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n+1; j++) {
-                cout << a[i][j] << " ";
-            } cout << endl;
-        }
-
-        // loop to perform gauss elimination
         for(int i = 0; i < n-1; i++) {
-            for(int k = i+1; k < n; k++) {
-                double t = a[k][i] / a[i][i];
-                for(int j = 0; j < n; j++) {
-                    a[k][j] -= (a[i][j]*t);
-                    // if(a[k][j] < 0.00001) a[k][j] = 0;
+
+            if(a[i][i] == 0) {
+                cout << "Invalid" << endl;
+                return;
+            }
+
+            for(int j = i+1; j < n; j++) {
+                double ratio = a[j][i] / a[i][i];
+
+                for(int k = 0; k < n+1; k++) {
+                    a[j][k] -= (ratio*a[i][k]);
                 }
             }
         }
 
-        // matrix after gauss elimination
+        // Back substitution
+        double x[100];
 
-        cout << "Matrix after gauss elimination: " << endl;
- 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n+1; j++) {
-                cout << a[i][j] << " ";
-            } cout << endl;
-        }
+        x[n-1] = a[n-1][n] / a[n-1][n-1];
 
-
-        // back-substitution
-        double ans[n];
-        for(int i = n-1; i >= 0; i--) {
-            ans[i] = a[i][n];
-
-            for(int j = 0; j < n; j++) {
-                if(j != i) {
-                    ans[i] -= (a[i][j] * ans[j]);
-                    ans[i] /= a[i][i];
-                    // if(ans[i])
-                }
+        for(int i = n-2; i >= 0; i--) {
+            x[i] = a[i][n];
+            for(int j = i+1; j < n; j++) {
+                x[i] -= (a[i][j] * x[j]);
             }
+            x[i] = x[i] / a[i][i];
         }
 
-        cout << "\nThe value of the variables are: " << endl;
-
+        // Display the values of x
         for(int i = 0; i < n; i++) {
-            cout << ans[i] << endl;
+            cout << "x[" << i << "] = " << x[i] << endl;
         }
-
     }
 
 };
 
 int main() {
 
-    Gauss_Elimination ge;
+    GaussElimination ge;
     ge.solve();
 
     return 0;
